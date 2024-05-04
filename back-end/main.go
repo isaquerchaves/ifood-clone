f2cc5fc7b33ea48f4426596b701db57cefb81c74
Isaque Chaves <isaque.ramosch@gmail.com>
2024-05-03 21:49:30 -0300

    feat: add Category router

--- back-end/main.go
+++ back-end/main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/isaquerchaves/ifood-clone/tree/main/back-end/config"
+	"github.com/isaquerchaves/ifood-clone/tree/main/back-end/controllers"
+)
+
+func init() {
+	config.ConnectToDb()
+}
+
 func main() {
-	println("hello world")
+	app := gin.Default()
+
+	// Middleware CORS
+	app.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	})
+
+	app.GET("categories", controllers.GetAllCategory)
+
+	app.Run("localhost:3002")
+
 }

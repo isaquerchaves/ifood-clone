31191bc5a287170df69472a528427ce908b96a62
Isaque Chaves <isaque.ramosch@gmail.com>
2024-05-03 21:48:56 -0300

    feat: add Category model

---
+++ back-end/models/modelCategory.go
@@ -0,0 +1,19 @@
+package models
+
+import "gorm.io/gorm"
+
+type Category struct {
+	gorm.Model
+	ID         string `gorm:"primaryKey"`
+	Name       string `gorm:"column:name"`
+	ImageUrl   string `gorm:"column:imageUrl"`
+	Deleted_at string `gorm:"column:deleted_at"`
+}
+
+func (Category) TableName() string {
+	return "Category"
+}
+
+func (Category) SchemaName() string {
+	return "public"
+}

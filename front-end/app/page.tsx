7e9efbfdcffc57740828688e809201eba656bf56
Isaque Chaves <isaque.ramosch@gmail.com>
2024-05-07 19:34:47 -0300

    feat: add promo banner 01

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Header from "./_components/header";
 import Search from "./_components/search";
 import CategoryList from "./_components/ui/category-list";
@@ -11,6 +12,18 @@ export default function Home() {
       </div>
       <div className="px-5 pt-6">
         <CategoryList />
+      </div>
+      
+      <div className="px-5 pt-6">
+        <Image 
+        src="/promo-banner-01.png"
+        alt="Até 30% de desconto em pizzas"
+        height={0}
+        width={0}
+        className="w-full h-auto object-contain"
+        sizes="100vw"
+        quality={100}
+      />
       </div>
       
     </>

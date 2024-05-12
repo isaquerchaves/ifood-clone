a43fb115a954eaed5ba0ac445acf71be3a5c4460
Isaque Chaves <isaque.ramosch@gmail.com>
2024-05-12 10:21:49 -0300

    feat: add restaurant relationship with the product

--- back-end/models/modelProduct.go
+++ back-end/models/modelProduct.go
@@ -1,20 +1,19 @@
 package models
 
-import (
-	"gorm.io/gorm"
-)
+import "gorm.io/gorm"
 
 type Product struct {
 	gorm.Model
-	ID                 string  `gorm:"primaryKey"`
-	Name               string  `gorm:"column:name"`
-	Description        string  `gorm:"column:description"`
-	ImageUrl           string  `gorm:"column:imageUrl"`
-	Price              float64 `gorm:"column:price"`
-	DiscountPercentage int32   `gorm:"column:discountPercentage"`
-	RestaurantId       string  `gorm:"column:restaurantId"`
-	CategoryId         string  `gorm:"column:categoryId"`
-	Deleted_at         string  `gorm:"column:deleted_at"`
+	ID                 string     `gorm:"primaryKey"`
+	Name               string     `gorm:"column:name"`
+	Description        string     `gorm:"column:description"`
+	ImageUrl           string     `gorm:"column:imageUrl"`
+	Price              float64    `gorm:"column:price"`
+	DiscountPercentage int32      `gorm:"column:discountPercentage"`
+	RestaurantID       string     `gorm:"column:restaurantId"`
+	CategoryID         string     `gorm:"column:categoryId"`
+	Deleted_at         string     `gorm:"column:deleted_at"`
+	Restaurant         Restaurant // Relacionamento um-para-um, cada produto pertence a um restaurante
 }
 
 func (Product) TableName() string {

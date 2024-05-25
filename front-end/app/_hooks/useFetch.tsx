d766afc534408c595dadc9b6ea881461684251aa
Isaque Chaves <isaque.ramosch@gmail.com>
2024-05-25 17:12:31 -0300

    feat: add loading and error state

--- front-end/app/_hooks/useFetch.tsx
+++ front-end/app/_hooks/useFetch.tsx
@@ -1,56 +1,89 @@
 import { useEffect, useState } from "react";
 import { fetchCategories, Category, Product, fetchProducts, fetchRestaurants, Restaurant } from "@/services/service";
 
-export function useFetchCategories(): Category[] {
-  const [categories, setCategories] = useState<Category[]>([]);
-
-  useEffect(()=> {
-    async function fetchData() {
-      try {
-        const categoriesData = await fetchCategories();
-        setCategories(categoriesData);
-      } catch (error) {
-        console.error('Erro ao buscar categorias:', error);
-      }
-    }
-    fetchData();
-  }, []);
-
-  return categories;
+interface UseFetchProductsResult {
+  products: Product[];
+  loading: boolean;
+  error: Error | null;
 }
 
-export function useFetchProducts(): Product[] {
+export function useFetchProducts(): UseFetchProductsResult {
   const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<Error | null>(null);
 
-  useEffect(()=> {
+  useEffect(() => {
     async function fetchData() {
       try {
         const productsData = await fetchProducts();
         setProducts(productsData);
       } catch (error) {
-        console.error('Erro ao buscar categorias:', error);
+        setError(error as Error);
+      } finally {
+        setLoading(false);
       }
     }
+
     fetchData();
   }, []);
 
-  return products;
+  return { products, loading, error };
 }
 
-export function useFetchRestaurants(): Restaurant[] {
-  const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
+interface UseFetchCategoriesResult {
+  categories: Category[];
+  loading: boolean;
+  error: Error | null;
+}
 
-  useEffect(()=> {
+export function useFetchCategories(): UseFetchCategoriesResult {
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const categoriesData = await fetchCategories();
+        setCategories(categoriesData);
+      } catch (error) {
+        setError(error as Error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  return { categories, loading, error };
+}
+
+interface UseFetchRestaurantsResult {
+  restaurants: Restaurant[];
+  loading: boolean;
+  error: Error | null;
+}
+
+export function useFetchRestaurants(): UseFetchRestaurantsResult {
+  const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
     async function fetchData() {
       try {
         const restaurantsData = await fetchRestaurants();
         setRestaurants(restaurantsData);
       } catch (error) {
-        console.error('Erro ao buscar categorias:', error);
+        setError(error as Error);
+      } finally {
+        setLoading(false);
       }
     }
+
     fetchData();
   }, []);
 
-  return restaurants;
+  return { restaurants, loading, error };
 }

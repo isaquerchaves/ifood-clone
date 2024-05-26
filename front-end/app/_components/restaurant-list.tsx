cdfae883866a149c580354711f803d1c281d749b
Isaque Chaves <isaque.ramosch@gmail.com>
2024-05-26 17:20:03 -0300

    feat: add redirect from restaurant page

--- front-end/app/_components/restaurant-list.tsx
+++ front-end/app/_components/restaurant-list.tsx
@@ -1,16 +1,19 @@
+import Link from "next/link";
 import { useFetchRestaurants } from "../_hooks/useFetch";
 import RestaurantItem from "./restaurant-item";
 
 const RestaurantList = () => {
   const { restaurants } = useFetchRestaurants();
 
-  return ( 
-    <div className="flex gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden px-5">
-      {restaurants.map((restaurant:any) => (
-        <RestaurantItem key={restaurant.ID} restaurant={restaurant} />
-      ))}
-    </div>
-   );
-}
- 
+  return (
+      <div className="flex gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden px-5">
+        {restaurants.map((restaurant: any) => (
+          <Link href={`/restaurants/${restaurant.ID}`}>
+          <RestaurantItem key={restaurant.ID} restaurant={restaurant} />
+          </Link>
+        ))}
+      </div>
+  );
+};
+
 export default RestaurantList;

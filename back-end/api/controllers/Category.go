3054a52c4224e07219feeb2b3ecaaa5a29883c66
Isaque Chaves <isaque.ramosch@gmail.com>
2024-06-17 16:58:04 -0300

    teste 3

--- back-end/api/controllers/Category.go
+++ back-end/api/controllers/Category.go
@@ -1,12 +1,14 @@
 package controllers
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/isaquerchaves/ifood-clone/tree/main/back-end/api/config"
 	"github.com/isaquerchaves/ifood-clone/tree/main/back-end/api/models"
 )
 
-func GetAllCategory(c *gin.Context) {
+func GetAllCategory(c *gin.Context, w http.ResponseWriter, r *http.Request) {
 	var categories []models.Category
 
 	config.DB.Preload("Product").Find(&categories)
--- back-end/api/controllers/CategoryToRestaurant.go
+++ back-end/api/controllers/CategoryToRestaurant.go
@@ -1,12 +1,14 @@
 package controllers
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/isaquerchaves/ifood-clone/tree/main/back-end/api/config"
 	"github.com/isaquerchaves/ifood-clone/tree/main/back-end/api/models"
 )
 
-func GetAllCategoryToRestaurant(c *gin.Context) {
+func GetAllCategoryToRestaurant(c *gin.Context, w http.ResponseWriter, r *http.Request) {
 	var categories []models.CategoryToRestaurant
 	config.DB.Preload("Category").Find(&categories)
 

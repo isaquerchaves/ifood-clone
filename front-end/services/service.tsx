60ccd4a03fbb42bbcf999f35ec63fe98514b7600
Isaque Chaves <isaque.ramosch@gmail.com>
2024-06-17 12:17:27 -0300

    feat: changed API_BASE_URL to Render URL

--- front-end/services/service.tsx
+++ front-end/services/service.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3002";
+const API_BASE_URL = "https://ifood-clone-u3t1.onrender.com";
 
 export interface Category {
   ID: string;

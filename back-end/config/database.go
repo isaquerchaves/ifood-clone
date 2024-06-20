37b4947ea6fc64163f9364bda9bc63edd332b736
Isaque Chaves <isaque.ramosch@gmail.com>
2024-06-20 11:59:26 -0300

    fix: Load .env file only in non-production environments

--- back-end/config/database.go
+++ back-end/config/database.go
@@ -1,8 +1,10 @@
 package config
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
@@ -19,17 +21,24 @@ func ConnectToDb() {
 		}
 	}
 
-	dsn := "host=" + os.Getenv("DB_HOST") +
-		" user=" + os.Getenv("DB_USER") +
-		" password=" + os.Getenv("DB_PASSWORD") +
-		" dbname=" + os.Getenv("DB_NAME") +
-		" port=" + os.Getenv("DB_PORT") +
-		" sslmode=" + os.Getenv("DB_SSLMODE")
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	portStr := os.Getenv("DB_PORT")
+	sslmode := os.Getenv("DB_SSLMODE")
+
+	port, parseErr := strconv.ParseUint(portStr, 10, 32)
+	if parseErr != nil {
+		log.Fatalf("Failed to parse port: %v", parseErr)
+	}
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=%s", host, user, password, dbname, port, sslmode)
 
 	// Conectar ao banco de dados
-	var err error
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatal("Failed to connect to the database")
+	var connectErr error
+	DB, connectErr = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if connectErr != nil {
+		log.Fatalf("Failed to connect to the database: %v", connectErr)
 	}
 }

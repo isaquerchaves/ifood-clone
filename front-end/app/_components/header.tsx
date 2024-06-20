0a87b21fd99998c97b441ddce9024afae72223eb
Isaque Chaves <isaque.ramosch@gmail.com>
2024-06-20 10:09:31 -0300

    feat: add card to home page header

--- front-end/app/_components/header.tsx
+++ front-end/app/_components/header.tsx
@@ -1,9 +1,15 @@
+"use client"
 import Image from "next/image";
 import { Button } from "./ui/button";
-import { MenuIcon } from "lucide-react";
+import { ShoppingCart } from "lucide-react";
 import Link from "next/link";
+import { useState } from "react";
+import { Sheet, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet";
+import Cart from "./cart/cart";
 
 const Header = () => {
+  const [isCartOpen, setIsCartOpen ] = useState(false);
+
   return (
     <div className="flex justify-between pt-6 px-5">
       <Link href="/">
@@ -14,9 +20,20 @@ const Header = () => {
         size="icon"
         variant="outline"
         className="border-none bg-transparent"
+        onClick={() => setIsCartOpen(true)}
       >
-        <MenuIcon />
+        <ShoppingCart />
       </Button>
+
+      <Sheet open={isCartOpen} onOpenChange={setIsCartOpen}>
+          <SheetContent className="w-[90vw]">
+            <SheetHeader>
+              <SheetTitle className="text-left">Sacola</SheetTitle>
+            </SheetHeader>
+
+            <Cart />
+          </SheetContent>
+        </Sheet>
     </div>
   );
 };
